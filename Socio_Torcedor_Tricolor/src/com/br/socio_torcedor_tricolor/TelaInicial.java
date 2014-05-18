787305a5f3af3e7e81228d236bc4967175494a9e
demismg72 <demismg72@gmail.com>
2014-05-18 18:04:40 +0000

    Tela Inicial com botões e campo para digitar código

--- Socio_Torcedor_Tricolor/src/com/br/socio_torcedor_tricolor/TelaInicial.java
+++ Socio_Torcedor_Tricolor/src/com/br/socio_torcedor_tricolor/TelaInicial.java
@@ -1,18 +1,27 @@
 package com.br.socio_torcedor_tricolor;
 
+import com.br.socio_torcedor_tricolor.gui.TelaEnviarCodigo;
+
 import bd.Banco;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class TelaInicial extends Activity {
+public class TelaInicial extends Activity implements OnClickListener {
 
+	Button btnTelaEnviarCodigo;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_inicial);
 		Banco banco=new Banco (TelaInicial.this);
 		banco.criarBanco();
+		btnTelaEnviarCodigo=(Button) findViewById(R.id.btnTelaEnviarCodigo);
+		btnTelaEnviarCodigo.setOnClickListener(this);
 	}
 
 	@Override
@@ -20,6 +29,16 @@ public class TelaInicial extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tela_inicial, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		if (v.getId()==R.id.btnTelaEnviarCodigo){
+			Intent intent=new Intent(TelaInicial.this, TelaEnviarCodigo.class);
+			startActivity(intent);
+		}
+		
 	}
 
 }
