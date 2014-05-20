1d34eec09a0c601a59f42ef60fa05118abc98b9f
demismg72@gmail.com <demismg72@gmail.com@bc522815-9da4-804f-780b-72ba3fe72839>
2014-05-20 03:27:05 +0000

    BAckground e telas de login, loja e perfil

---
+++ trunk/Socio_Torcedor_Tricolor/src/com/br/socio_torcedor_tricolor/gui/TelaPerfil.java
@@ -0,0 +1,26 @@
+package com.br.socio_torcedor_tricolor.gui;
+
+import com.br.socio_torcedor_tricolor.R;
+import com.br.socio_torcedor_tricolor.R.layout;
+import com.br.socio_torcedor_tricolor.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TelaPerfil extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tela_perfil);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tela_perfil, menu);
+		return true;
+	}
+
+}
